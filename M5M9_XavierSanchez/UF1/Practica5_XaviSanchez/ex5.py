5c1c3609c3f077af643028b53809173c949db327
XavierSanchezAlcantara <xavier.sanchez.alcantara@gmail.com>
2019-10-22 19:54:09 +0200

    asda

--- M5M9_XavierSanchez/UF1/Practica5_XaviSanchez/ex5.py
+++ M5M9_XavierSanchez/UF1/Practica5_XaviSanchez/ex5.py
@@ -1,5 +1,8 @@
-import sys
+#-*- coding: utf-8 -*-
 
+"""
+Modul per printar els numeros parells 
+"""
 class llista_primers:
     def __init__(self, n):
         self.n = n
@@ -7,10 +10,15 @@ class llista_primers:
         self.busca()
 
     def busca(self):
+        """
+        Metode per buscar el numero i veure si es parell
+        """
         if (len(self.llista) == 0):
+            # Llargaria llista es igual a 0 
             self.llista.append(2)
             self.busca()
         elif (len(self.llista) < self.n):
+            # Llargaria llista es menor al numero introduit
             trobat = False
             seguent = self.llista[-1]+1
             while not trobat:
