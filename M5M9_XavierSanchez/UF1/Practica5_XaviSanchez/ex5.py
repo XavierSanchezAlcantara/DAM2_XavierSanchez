8ab7dc835053bf25d4fdc78b4f09775da49e50ac
XavierSanchezAlcantara <xavier.sanchez.alcantara@gmail.com>
2019-10-23 18:20:46 +0200

    asa

--- M5M9_XavierSanchez/UF1/Practica5_XaviSanchez/ex5.py
+++ M5M9_XavierSanchez/UF1/Practica5_XaviSanchez/ex5.py
@@ -23,16 +23,21 @@ class llista_primers:
             seguent = self.llista[-1]+1
             while not trobat:
                 for i in self.llista:
+                    #recorre tots els numeros de la llista
                     if seguent%i == 0:
+                        #Saber si el numero es par o impar, sabent si el residu es 0 o 1.
                         seguent += 1
                         trobat = False
                         break
                     else:
                         trobat = True
+            
             self.llista.append(seguent)
             self.busca()
+            #S'introdueix el seguent valor a la llista i executa la funcio busca de nou
 
 
 if __name__ == '__main__':
     l = llista_primers(int(sys.argv[1]))
+    # el usuari te que introduir un parametre si o si
     print l.llista
