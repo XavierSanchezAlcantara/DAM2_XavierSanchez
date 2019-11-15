7e23df4576c5cb3d301517f03938944eb28e432d
XavierSanchezAlcantara <xavier.sanchez.alcantara@gmail.com>
2019-11-15 19:50:05 +0100

    asda

---
+++ M7M8_XavierSanchez/NF2_EX2/Exercici netas/js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $(".boton").on("click", function () {
+        $("#formulari").slideToggle();
+
+    });
+
+});
+
+$(document).ready(function () {
+    $("#botonNoticia").on("click", function () {
+        var value=$('#select').val();
+        if(value=="Principal"){
+            $("aside").prepend("<article>"+"<h3>"+($("#titulo").val()+"<button class='fa fa-trash fa-lg'></i></h3><p>"+$("#textArea").val())+"</p><article>");
+        }else if(value=="Secundari"){
+            $("section").prepend("<article>"+"<h2>"+($("#titulo").val()+"<button class='fa fa-trash fa-lg'></i></h2><p>"+$("#textArea").val())+"</p><article>");
+        }else{
+            alert('No has seleccionat el tipus de noticia!!!!!')
+        }
+        
+        
+    
+
+        
+    });
+
+
+});
+$(document).ready(function () {
+    $("#fa fa-trash fa-lg").on("click", function () {
+        $(this.parent().parent().html(""));
+
+    });
+
+});
