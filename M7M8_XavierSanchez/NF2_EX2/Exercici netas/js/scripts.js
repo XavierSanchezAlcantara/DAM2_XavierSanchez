f4544e6909d4fc4e5ffe396c232d7ca4d247c3b5
XavierSanchezAlcantara <xavier.sanchez.alcantara@gmail.com>
2019-11-18 19:56:36 +0100

    asda

--- M7M8_XavierSanchez/NF2_EX2/Exercici netas/js/scripts.js
+++ M7M8_XavierSanchez/NF2_EX2/Exercici netas/js/scripts.js
@@ -8,22 +8,30 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $("#botonNoticia").on("click", function () {
-        var value=$('#select').val();
-        if(value=="Principal"){
-            $("aside").prepend("<article>"+"<h3>"+($("#titulo").val()+"<button class='fa fa-trash fa-lg'></i></h3><p>"+$("#textArea").val())+"</p><article>");
-        }else if(value=="Secundari"){
-            $("section").prepend("<article>"+"<h2>"+($("#titulo").val()+"<button class='fa fa-trash fa-lg'></i></h2><p>"+$("#textArea").val())+"</p><article>");
-        }else{
+        var d =new Date();
+        var newArray = [$("#titulo").val(),$("#textArea").val()];
+
+
+
+
+        var value = $('#select').val();
+        if (value == "Principal") {
+            $("aside").prepend("<article>" + "<h3>" + ($("#titulo").val() + "<button class='fa fa-trash fa-lg'></button></h3><p>" + $("#textArea").val()) + "</p><article>");
+        } else if (value == "Secundari") {
+            $("section").prepend("<article>" + "<h2>" + ($("#titulo").val() + "<button class='fa fa-trash fa-lg'></button></h2><p>" + $("#textArea").val()) + "</p><article>");
+        } else {
             alert('No has seleccionat el tipus de noticia!!!!!')
         }
-        
-        
-    
-
-        
     });
     $(".fa-trash").click(function () {
         $(this).parent().parent().hide();
     });
 
+    if (localStorage.numvisites) {
+        localStorage.numvisites = Number(localStorage.numvisites) + 1;
+    } else {
+        localStorage.numvisites = 1;
+    }
+    $("#result").html( "Ens has visitat " + JSON.parse(localStorage.numvisites) + " vegades.");
+   
 });
