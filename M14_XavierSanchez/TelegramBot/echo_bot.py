94f4b8b2f4a411074e40991ec267e55662c74279
XavierSanchezAlcantara <xavier.sanchez.alcantara@gmail.com>
2019-11-20 18:04:09 +0100

    ad

--- M14_XavierSanchez/TelegramBot/echo_bot.py
+++ M14_XavierSanchez/TelegramBot/echo_bot.py
@@ -7,20 +7,25 @@ import sys
 from random import randint
 reload(sys)
 sys.setdefaultencoding('utf-8')
+fitxer_no_us=True
 bot = telebot.TeleBot("940457893:AAH6J8fRSRMrMlMbm5zbYdjeliz51dAK944")
 
 
-
+global var1,var2  
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Tens que seleccionar una d'aquestes tres opcions:  /play, /help ")
-    f = open("historial.txt", "a") 
+    f = open("historial.txt", "r+") 
 
     if (str(message.chat.id)) not in f:
         print(message.chat.id)
+        f.write(str(message.chat.id)+',0,0\n')
+        
         f.close()
     else:
-        f.write(str(message.chat.id)+"\n")
+        
+        
+        print("en el fixero")
         f.close()
         pass
 
@@ -45,9 +50,12 @@ def send_joc(message):
     elif a==2:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Guanyat")
+        win(message.chat.id)
+
     else:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Perdut")
+        lose(message.chat.id)
         pass
     
 
@@ -60,9 +68,11 @@ def send_joc1(message):
     if a==0:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Guanyat")
+        win(message.chat.id)
     elif a==2:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Perdut")
+        lose(message.chat.id)
     else:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Empatat")
@@ -78,15 +88,18 @@ def send_joc2(message):
     if a==0:
         bot.send_message(message.chat.id, llista[a])
         bot.reply_to(message.chat.id, "Has Perdut")
+        lose(message.chat.id)
     elif a==2:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Guanyat")
+        win(message.chat.id)
     else:
         bot.send_message(message.chat.id, llista[a])
         bot.send_message(message.chat.id, "Has Empatat")
         pass
     
     pass
+
 @bot.message_handler(commands=['historial'])
 def send_history(message):
     f2= open("historial.txt", "r")
@@ -94,5 +107,33 @@ def send_history(message):
     bot.send_message(message.chat.id, f2.read())
     f2.close()
     pass
+
+"""
+def fitxer_en_us():
+    if fitxer_no_us:
+        fitxer_no_us=False
+        fitxer_no_us=True
+
+    else:
+        print()
+"""
+def win(usuari):
+    fr= open("historial.txt","r+")
+    for x in fr.readlines() :
+        l=fr.readline()
+        if (str(usuari))  in l:
+            var1=int(l.split(",")[1])
+            var1+=1 
+        fr.write(str(usuari)+","+str(var1)+",0\n")
+    fr.close()
+def lose(usuari):
+    fr= open("historial.txt","r+")
+    for x in fr.readlines() :
+        l=fr.readline()
+        if (str(usuari))  in l:
+            var2=int(l.split(",")[2])
+            var2+=1
+    fr.write(str(usuari)+','+str(var2)+',0\n')
+    fr.close()  
 
 bot.polling()
