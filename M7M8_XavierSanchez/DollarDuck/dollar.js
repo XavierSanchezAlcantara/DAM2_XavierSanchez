ac26982249227c418a5e2abfbd2caee35c8578b5
XavierSanchezAlcantara <xavier.sanchez.alcantara@gmail.com>
2019-12-12 19:47:14 +0100

    googlepapi

---
+++ M7M8_XavierSanchez/DollarDuck/dollar.js
@@ -0,0 +1,9 @@
+$(function(){
+    var url2= "https://sheets.googleapis.com/v4/spreadsheets/1o2dz1wVN-Nnj4Ut1dVffUx_anXFlmlUWHZP1ppeo4iQ/values/";
+	url2 += "xaviDB!A1:B1?majorDimension=ROWS&key=AIzaSyCPyzW_SVgl6ZDNO09f8Zq3eCNYNZCTC3M";
+
+    $.get(url2,function(dades){
+        
+        console.log(JSON.stringify(dades));
+    });
+});
